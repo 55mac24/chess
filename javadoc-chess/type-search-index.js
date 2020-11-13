29707bfe290fcd241e525096a326eb19f33b5abc
55mac24 <macauleypinto@gmail.com>
2020-11-13 11:46:26 -0500

    Java Chess Documentation

---
+++ javadoc-chess/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Chess"},{"p":"<Unnamed>","l":"ChessBoard"},{"p":"<Unnamed>","l":"ChessPiece"},{"p":"<Unnamed>","l":"Point"}]
